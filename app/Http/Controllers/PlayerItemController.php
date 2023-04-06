d1ccea9021568852ee14994a4a0962834be03809
Fukuda Kouki <kouki.fukuda@sorcerer.co.jp>
2023-04-06 14:31:17 +0900

    where文 を使い特定のレコードの情報のみ取得するように変更

--- app/Http/Controllers/PlayerItemController.php
+++ app/Http/Controllers/PlayerItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Arr;
 use App\Models\PlayerItem;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,30 +12,28 @@ class PlayerItemController extends Controller
     //
     public function addItem(Request $request, $id)
     {
-        $playerItemId = [];
-        $data = PlayerItem::get();
-        $itemId = $request->input('itemId');
-        
-        //同じ持ち物のデータがある場合はそのレコードの'count'に追加するアイテムの個数分インクリメント
-        foreach ($data as $value) {
-            if ($value['playerId'] == $id || $value['itemId'] == $itemId) 
-            {
-                PlayerItem::find($value['id'])->increment('count',$request->input(('count')));
-                $playerItemId = $value['id'];
-                break;
-            }
-        }
+        $data = PlayerItem::query()->
+        where([['player_id', $id],['item_id', $request->input('itemId')]])->
+        get();
         
         //同じ持ち物のデータがない場合新しくレコードを作成する
-        if(!$playerItemId)
+        if($data->isEmpty())
         {
-            $playerItemId = PlayerItem::insertGetId([
-                'playerId' => $id,
-                'itemId' => $request->input('itemId'),
+            PlayerItem::insert([
+                'player_id' => $id,
+                'item_id' => $request->input('itemId'),
                 'count' => $request->input('count')
-                ]);
+            ]);
+        }
+        else //同じ持ち物のデータがある場合はそのレコードの'count'に追加するアイテムの個数分インクリメント
+        {
+            PlayerItem::query()->
+            where([['player_id', $id],['item_id', $request->input('itemId')]])->
+            increment('count', $request->input('count'));
         }
             
-        return new Response(PlayerItem::find($playerItemId,['itemId','count']));
+        return new Response(PlayerItem::query()->
+        where([['player_id', $id],['item_id', $request->input('itemId')]])->
+        select('item_id','count')->get());
     }
 }
