c9c3674726be9a1fbc0fe9ef9c04ba6ca5ddd567
fukuda-kouki <129708454+fukuda-kouki@users.noreply.github.com>
2023-04-07 16:01:31 +0900

    useItem関数の実装 (#7)
    * useItem関数の実装
    
    * 定数宣言の追加、アイテムの複数個使用の処理、アイテムの所持数が0のレコードの削除処理の実装
    
    * 最大まで回復したらそれ以上アイテムを使用しないように修正
    
    * レコードの削除部分の消去、アイテムの効果量の呼び出しをループの外に移動
    
    * アイテムの効果量の呼び出しを一度にまとめた
    
    * count変数のスコープの修正

--- app/Http/Controllers/PlayerItemController.php
+++ app/Http/Controllers/PlayerItemController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Arr;
 use App\Models\PlayerItem;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\Player;
+use App\Models\Item;
 
 class PlayerItemController extends Controller
 {
-    //
+
+    //プレイヤーのステータス最大値の定数を宣言
+    const MAX_STATUS = 200;
+
+    //アイテム追加関数
     public function addItem(Request $request, $id)
     {
         $data = PlayerItem::query()->
@@ -39,6 +44,65 @@ class PlayerItemController extends Controller
         return new Response([
             'itemId'=> $playerItemData->value('item_id'),
             'count' => $playerItemData->value('count')
+        ]);
+    }
+
+    //アイテム使用関数
+    public function useItem(Request $request, $id)
+    {
+        //該当するデータの検索
+        $data = PlayerItem::query()->
+        where([['player_id', $id],['item_id', $request->input('itemId')]]);
+
+        $count = 0;
+        if($request->has('count'))
+        {
+            $count = $request->input('count');
+        }
+        else //リクエストに'count'が含まれていなければ使用数を1にする
+        {
+            $count = 1;
+        }
+        
+        //所持数が使用数以下もしくはゼロ（該当するレコードがない）の場合エラー
+        if($data->get()->isEmpty() || $data->value('count') < $count)
+        {
+            return new Response("アイテムを{$count}個所持していません。",Response::HTTP_BAD_REQUEST);
+        }
+
+        $playerData = Player::where('id',$id);
+        $useablecount = 0;
+        $dataArr =['hp', 'mp'];
+
+        //ステータスが上限に達するまでに使用できるアイテムの個数を数える
+        $tempValue = $playerData->value($dataArr[$data->value('item_id') - 1]);
+        $effectValue = Item::where('id', $request->input('itemId'))->value('value');
+        while($tempValue < PlayerItemController::MAX_STATUS)
+        {
+            $tempValue += $effectValue;
+            $useablecount++;
+        }
+
+        //使用可能個数がゼロ以上の場合に使用
+        if($useablecount > 0)
+        {
+            //実際に使用する個数の決定
+            $useCount = min($count,$useablecount);
+
+            //アイテムを使用する
+            $value = $playerData->value($dataArr[$request->input('itemId') - 1]) + $effectValue * $useCount;
+            $playerData->update([$dataArr[$request->input('itemId') - 1] => min($value, PlayerItemController::MAX_STATUS)]);
+            $data->decrement('count', $useCount);
+        }
+
+        return new Response([
+            'itemId' => $request->input('itemId'),
+            'count' => $data->value('count'),
+            'player' => [
+                'id' => $id,
+                'hp' => $playerData->value('hp'),
+                'mp' => $playerData->value('mp')
+            ]
         ]);
     }
 }
